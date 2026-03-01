acf8c5b85304413c4e1f083c3ea2fda7d6dc0542
Дід Alik <didalik.aim@gmail.com>
2026-03-01 20:44:56 +0000

    Add basic functionality

--- demo_tm.mjs
+++ demo_tm.mjs
@@ -1,12 +1,107 @@
 import { put, reset, } from './lib/util.mjs' // {{{1
+import { addStream, makeBuyOffer, trustAssets, } from '../lib/sdk.mjs'
 import { Asset, Keypair, Horizon, Networks, TransactionBuilder, } from '@stellar/stellar-sdk'
 
 const out = m => typeof m == 'string' ? put( // {{{1
   `<div style='text-align: right'><b>${m}</b></div>`
 ) : (console.log(m.message), put(m.message))
 
-reset({ content: document.getElementById('content1'), }) // {{{1
+reset({ content: document.getElementById('content1'), handleCtrlC }) // {{{1
 put(`Delivered ${location} on ${Date()} to YOUR_IP_ADDRESS`, '<hr/>')
 
 let MA = new Asset('MA', 'MA_IssuerPK')
-out(MA.toString())
+
+let vm = { // {{{1
+  s: [],
+  e: { log: console.log, nw: Networks.TESTNET,
+    server: new Horizon.Server("https://horizon-testnet.stellar.org"),
+  },
+  c: {},
+  d: { MA: new Asset('MA', 'MA_IssuerPK'), XLM: new Asset('XLM', null), }
+}
+let ma = vm.d.MA
+out(`Presently using ${ma.getCode()}-${ma.getIssuer()}`)
+let secret = Keypair.random().secret() // TODO use localStorage
+console.log(secret)
+
+let kp = Keypair.fromSecret(secret); vm.d.kp = kp // {{{1
+let pk = kp.publicKey()
+if (!vm.c.done) {
+  out('Loading your Stellar TESTNET account... ')
+  vm.e.server.loadAccount(pk).then(account => loaded.call(vm, account))
+  .catch(error => {
+    if (!vm.c.done) {
+      out(`${error.message}.`)
+      if (error.message == 'Not Found') {
+        out('Creating your Stellar TESTNET account... ')
+        fetch(`https://friendbot.stellar.org?addr=${encodeURIComponent(pk)}`).
+        then(response => response.json()).then(json => {
+          console.log('new TESTNET account created: txId', json.id)
+          out('new account created.')
+          out('Loading your Stellar TESTNET account... ')
+        }).then(_ => vm.e.server.loadAccount(pk)).
+        then(account => loaded.call(vm, account)).
+        catch(error => console.error(error))
+      }
+    }
+  })
+}
+
+function loaded (account) { // {{{1
+  let { s, e, c, d } = this
+  out('loaded.')
+  if (account.balances.length == 1) {
+    out('Updating your trustline... ')
+    trustAssets.call(this, account, d.kp, '10000', d.MA).then(txId => {
+      e.log('trustline updated: txId', txId)
+    }).then(_ => e.server.loadAccount(d.kp.publicKey())).then(account => {
+      out('updated.')
+      run.call(this, account)
+    })
+  } else {
+    run.call(this, account)
+  }
+}
+
+function run (account) { // {{{1
+  let { s, e, c, d } = this
+  let trade = effect => {
+    e.log('run trade effect', effect)
+    out('Demo request GRANTED')
+    handleCtrlC()
+    out('DONE')
+  }
+  d.account = account
+  addStream.call(this,
+    "user's trading effects",
+    [
+      ['trade', trade]
+    ],
+    account.id //, true
+  )
+}
+
+let ob = vm.e.server.orderbook(vm.d.MA, vm.d.XLM).cursor('now') // {{{1
+vm.s.push({
+  close: ob.stream({
+    onerror:   e => { throw e; },
+    onmessage: e => {
+      console.dir(e, { depth: null })
+      vm.e.log(vm)
+      if (vm.d.offerMade || !vm.d.account) {
+        return;
+      }
+      makeBuyOffer.call(vm, vm.d.kp, vm.d.account, vm.d.MA, vm.d.XLM, '1', '1').
+        then(_ => { vm.d.offerMade = true })
+    }
+  }),
+  tag: 'orderbook',
+})
+
+function handleCtrlC () { // {{{1
+  for (let stream of vm.s) {
+    stream.close()
+    out(`stream "${stream.tag}" closed`)
+  }
+}
+

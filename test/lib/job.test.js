ad2c0b7715db67d83ad2505de05d0d7bb07dade6
Дід Alik <didalik.aim@gmail.com>
2026-05-02 17:23:01 +0000

    Call startMonitor

--- test/lib/job.test.js
+++ test/lib/job.test.js
@@ -1,6 +1,6 @@
 import test from 'ava'; // {{{1
 import vault from '../../lib/vault.js'
-import { Demo, DemoSign, DemoTmReset, DemoTmUse, } from '../../lib/job.js'
+import { Demo, DemoSign, DemoTmReset, DemoTmUse, DemoTmUseRequest, } from '../../lib/job.js'
 
 test.serial('reset test monitor', t => { // {{{1
   t.timeout(90000)
@@ -9,10 +9,15 @@ test.serial('reset test monitor', t => { // {{{1
   })
 })
 
-test.serial('use test monitor', t => { // {{{1
+test.serial('use test monitor, 3 users', t => { // {{{1
   t.timeout(10000)
-  return DemoTmUse().then(r => {
-    t.is(r, 'OK')
+  let users = ['user1', 'user2', 'user3'], requests = [], opts = Promise.withResolvers()
+  for (const name of users) {
+    requests.push(DemoTmUseRequest({ name }))
+  }
+  Promise.all(requests).then(_ => opts.resolve('ok'))
+  return DemoTmUse(opts).then(r => {
+    t.is(r, 'ok')
   })
 })
 

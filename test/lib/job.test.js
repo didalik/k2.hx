eb939c8d613fdcba2b4e43be38d49dd11e2d0008
Дід Alik <didalik.aim@gmail.com>
2026-05-03 21:17:11 +0000

    Add function setupAccount (name)

--- test/lib/job.test.js
+++ test/lib/job.test.js
@@ -9,16 +9,13 @@ test.serial('reset test monitor', t => { // {{{1
   })
 })
 
-test.serial('use test monitor, 3 users', t => { // {{{1
-  t.timeout(10000)
-  let users = ['user1', 'user2', 'user3'], requests = [], opts = Promise.withResolvers()
-  for (const name of users) {
-    requests.push(DemoTmUseRequest({ name }))
-  }
-  Promise.all(requests).then(_ => opts.resolve('ok'))
+test.serial('use test monitor', t => { // {{{1
+  t.timeout(90000)
+  let opts = Promise.withResolvers()
+  DemoTmUseRequest({ name: 'user1' }).then(_ => (console.log(_), opts.resolve('ok')))
   return DemoTmUse(opts).then(r => {
     t.is(r, 'ok')
-  })
+  });
 })
 
 test.serial('unit-test DemoSign', t => { // {{{1

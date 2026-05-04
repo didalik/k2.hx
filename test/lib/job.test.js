5aeb618e2974a805ff3c957a9d4e668e8134f49a
Дід Alik <didalik.aim@gmail.com>
2026-05-04 13:48:00 +0000

    Change user1's trust

--- test/lib/job.test.js
+++ test/lib/job.test.js
@@ -2,18 +2,25 @@ import test from 'ava'; // {{{1
 import vault from '../../lib/vault.js'
 import { Demo, DemoSign, DemoTmReset, DemoTmUse, DemoTmUseRequest, } from '../../lib/job.js'
 
+let accounts // {{{1
+
 test.serial('reset test monitor', t => { // {{{1
   t.timeout(90000)
   return DemoTmReset({ vault }).then(r => {
-    t.is(r, 'OK')
+    accounts = r
+    t.is(vault.get('accounts.setup'), 'DONE')
   })
 })
 
 test.serial('use test monitor', t => { // {{{1
   t.timeout(90000)
-  let opts = Promise.withResolvers()
-  DemoTmUseRequest({ name: 'user1' }).then(_ => (console.log(_), opts.resolve('ok')))
-  return DemoTmUse(opts).then(r => {
+  let opts1 = {
+    issuer: { id: accounts.issuer.id, },
+    name: 'user1',
+  }
+  let opts2 = Promise.withResolvers()
+  DemoTmUseRequest(opts1).then(_ => (/*console.log(_), */opts2.resolve('ok')))
+  return DemoTmUse(opts2).then(r => {
     t.is(r, 'ok')
   });
 })

13bae127be44c19afc4ad9520a59a4d7809ac0d0
Дід Alik <didalik.aim@gmail.com>
2026-05-04 22:01:49 +0000

    Make a trade

--- test/lib/job.test.js
+++ test/lib/job.test.js
@@ -13,13 +13,13 @@ test.serial('reset test monitor', t => { // {{{1
 })
 
 test.serial('use test monitor', t => { // {{{1
-  t.timeout(90000)
+  t.timeout(40000)
   let opts1 = {
     issuer: { id: accounts.issuer.id, },
     name: 'user1',
   }
   let opts2 = Promise.withResolvers()
-  DemoTmUseRequest(opts1).then(_ => (/*console.log(_), */opts2.resolve('ok')))
+  DemoTmUseRequest(opts1).then(_ => (console.log(_), opts2.resolve('ok')))
   return DemoTmUse(opts2).then(r => {
     t.is(r, 'ok')
   });

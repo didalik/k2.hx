7fea29d0246846782edd727849838cee6be63eb3
Дід Alik <didalik.aim@gmail.con>
2026-09-12 18:21:16 +0000

    Update trust in prod

--- hexa.mjs
+++ hexa.mjs
@@ -1,92 +1,22 @@
 import { put, reset, } from './lib/util.mjs' // {{{1
 import vault from './lib/vault.js'
-import { issuerEffect, setupActor, stopMonitor, } from './lib/util.js'
-import demouser from './src/demoit/demouser.js'
-import { Asset, StrKey, } from '@stellar/stellar-sdk'
-import { Jobs, JWT, generate_keypair, verifyPayload, } from '../../jf/public/lib/sdk.js'
+import { Asset, Keypair, StrKey, } from '@stellar/stellar-sdk'
 import { Channel, } from '../../lib/util.mjs'
 import { hXsdk, } from './lib/sdk.mjs'
-import { rs4d, } from './demo/Ann.js'
 
 let color = 'blue'; const out = m => typeof m == 'string' ? put( // {{{1
   `<div style='text-align: right; color: ${color}'>${m}</div>`
 ) : put(m.message?.replaceAll('\n', '<br/>').replaceAll(' ', '&nbsp'))
 
-const Demo = { // {{{1
-  Running: { // {{{2
-    handle: (context, event) => { // no event on first call
-      if (!event) {
-        setupJC(Demo, context)
-        return Demo.channel.receive().then(s =>
-          new JWT(s).setIssuer(Demo.client.iss, Demo.client.sk).
-          setAudience(Demo.aud).sign()
-        ).then(jwt => Demo.job.context.ws.send(jwt));
-      }
-      verifyPayload(event.message).then(payload => {
-        handlePayload(payload) && out({ message: payload.sub })
-      })
-    },
-  },
-  onclose: data => { // {{{2
-    let context = Demo.job.context
-    Demo.job.resolve(`- ${context.attachment.iss.name}: Demo DONE`)
-  },
-  onerror: null, // is never called
-  onmessage:  data => {
-    let context = Demo.job.context
-    context.state.handle(context, data)
-  },
-  prefix: context => `- ${context.attachment.iss.name}: mocking Demo job<br/>`,
-  prrSetup: Promise.withResolvers(),
-  prrStart: Promise.withResolvers(), prrStop: Promise.withResolvers(),
-  // }}}2
-}
-
-const IssuerSign = { // {{{1
-  Running: { // {{{2
-    handle: (context, event) => { // no event on first call
-      if (!event) {
-        setupJC(IssuerSign, context)
-        return IssuerSign.channel.receive().then(s =>
-          new JWT(s).setIssuer(IssuerSign.client.iss, IssuerSign.client.sk).
-          setAudience(IssuerSign.aud).sign()
-        ).then(jwt => IssuerSign.job.context.ws.send(jwt));
-      }
-      verifyPayload(event.message).then(payload => {
-        if (payload.sub.startsWith('signed ')) {
-          IssuerSign.prr.resolve(payload.sub.slice(7))
-          return;
-        }
-        out({ message: payload.sub })
-      })
-    },
-  },
-  onclose: data => { // {{{2
-    let context = IssuerSign.job.context
-    IssuerSign.job.resolve(`- ${context.attachment.iss.name}: IssuerSign DONE`)
-  },
-  onerror: null, // is never called
-  onmessage:  data => {
-    let context = IssuerSign.job.context
-    context.state.handle(context, data)
-  },
-  prefix: context => `<br/>- ${context.attachment.iss.name}: mocking IssuerSign job<br/>`, 
-  prrSetup: Promise.withResolvers(),
-  // }}}2
-}
-
-const params = new URLSearchParams(location.search) // {{{1
-const name = params.get('demouser') ?? crypto.randomUUID()
-window.process = { env: {
-  Networks_PUBLIC: null, // or 'hX' to use public network
-}}
-
-let audience = ['demo', 'issuer/sign'] // {{{1
-
 reset({ // {{{1
-  content: document.getElementById('content1'), handleCtrlC: stopIssuerSign,
+  content: document.getElementById('content1'), handleCtrlC: handleCtrlC,
 })
 put(`Delivered ${location} on ${Date()} to YOUR_IP_ADDRESS`, '<hr/>')
+
+window.process = { env: {
+  Networks_PUBLIC: 'hX'
+}}
+let sdk = hXsdk({ out, vault })
 
 document.getElementById('userSK').addEventListener('paste', (event) => {
   prepareAndSignStellarTx(event.clipboardData.getData('text'))
@@ -95,11 +25,19 @@ document.getElementById('userSK').addEventListener('paste', (event) => {
 function prepareAndSignStellarTx (userSK) { // {{{1
   if (StrKey.isValidEd25519SecretSeed(userSK)) {
     put('You pasted valid secret key.')
+  } else {
+    throw ERROR('You pasted INVALID secret key.')
   }
+  let userPK = Keypair.fromSecret(userSK).publicKey()
+  let recipientKeys = [userSK, userPK]
+  let issuer = { id: 'GC7BFT2ZXIQAU2GAYNODPVJV4OBFECV5L3NKO4RV5SHXFUR24M3BZNPY' }
+  return sdk.server.server.loadAccount(userPK).then(recipient => {
+    return sdk.transaction.changeTrust({ issuer, recipient, recipientKeys });
+  }).then(tx => out(tx.created_at + ': DONE.'));
 }
 
-function stopIssuerSign () { // {{{1
-  out('stopIssuerSign: stopping job IssuerSign...')
+function handleCtrlC () { // {{{1
+  out('handleCtrlC stub')
 }
 
 /* Preventing page reload {{{1
